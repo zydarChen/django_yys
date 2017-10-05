778432973b73372424e8dc5e29cf366ff650b68f
zydarChen <zydarChen@outlook.com>
2017-10-05 16:37:59 +0800

    css

--- django_yys/settings.py
+++ django_yys/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=sra@_#m3rpdg1pt=3u@%tng!x4vx(l5@9w32_dve=_bg+ardf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'yys.zydarChen.top', '39.108.130.183']
-
-# DEBUG = True
+# DEBUG = False
 #
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'yys.zydarChen.top', '39.108.130.183']
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
